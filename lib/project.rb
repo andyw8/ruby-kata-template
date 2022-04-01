a94b2a9f70adf82d49622903ebed8eb2ecf50f51
Andy Waite <andyw8@users.noreply.github.com>
2022-04-01 14:34:01 -0400

    Remove accidental content

--- lib/project.rb
+++ lib/project.rb
@@ -1,38 +1,4 @@
 class Project
   def initialize
-    # To set a breakpoint:
-    # binding.break
   end
-end
-
-namespace :a do
-  desc "b"
-  task c: :environment do
-    d
-  end
-end
-namespace : do
-  desc ""
-  task : :environment do
-  task : do
-    
-  end
-end
-  
-test "foo" do
-  # setup
-  # exercise
-  # verify
-end
-
-test "" do
-  # setup
-  # exercise
-  # verify
-end
-
-test "yy" do
-  # setup
-  # exercise
-  what
 end
