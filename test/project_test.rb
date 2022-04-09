4d424718020b581ff22cca1545fb3eeacef754d2
Andy Waite <andyw8@users.noreply.github.com>
2022-04-09 18:00:01 -0400

    Cleanup example

--- test/project_test.rb
+++ test/project_test.rb
@@ -3,8 +3,6 @@ require "project"
 
 class ProjectTest < ActiveSupport::TestCase
   test "example" do
-    p = stub("project")
-    Project.new
     assert true, "Example passes, tests are executing correctly"
   end
 end
